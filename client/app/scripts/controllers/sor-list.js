08c6e445b47e093ed12bd2dbbe2dff3c7000f97b
Nelson Chan <khchanel@gmail.com>
2014-06-24 17:52:50 +1000

    [sor] remove useless code in controller

--- client/app/scripts/controllers/sor-list.js
+++ client/app/scripts/controllers/sor-list.js
@@ -10,6 +10,4 @@
 angular.module('srScopingApp')
   .controller('SorListCtrl', ['$scope', 'Sor', function ($scope, Sor) {
     $scope.sors = Sor.query();
-    $scope.kitchen = Sor.get('KIT1234');
-    $scope.living = Sor.get('LIV6668');
   }]);
