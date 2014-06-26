abdbdd7843cfc7585fbff49b8fe9a282a54a9889
Nelson Chan <khchanel@gmail.com>
2014-06-26 14:31:36 +1000

    add get by SORCode to restService

--- client/app/scripts/services/rest-service.js
+++ client/app/scripts/services/rest-service.js
@@ -5,5 +5,5 @@ var API_SERVER = 'http://srconstruction.khchanel.com';
 angular.module('srScopingApp.restService', ['ngResource'])
     .factory('Sor', ['$resource',
         function ($resource) {
-            return $resource(API_SERVER + '/sor');
+            return $resource(API_SERVER + '/sor/:SORCode');
         }]);
