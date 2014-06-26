60a2a1918fb3c5e2c0b2910f878fc8b3c62c6a19
Nelson Chan <khchanel@gmail.com>
2014-06-26 14:02:23 +1000

    change api server to srconstruction.khchanel.com

--- client/app/scripts/services/rest-service.js
+++ client/app/scripts/services/rest-service.js
@@ -3,6 +3,5 @@
 angular.module('srScopingApp.restService', ['ngResource'])
     .factory('Sor', ['$resource',
         function ($resource) {
-            return $resource('http://khchanel.com:8080/sor');
-            //return $resource('scripts/sors.json');
+            return $resource('http://srconstruction.khchanel.com/sor');
         }]);
