a7e69ec300fed5ee7ab5dccf7573b2f5f949f7fb
Nelson Chan <khchanel@gmail.com>
2014-07-02 15:14:19 +1000

    [basket] use $storage and add clear basket function

--- client/app/scripts/controllers/basket.js
+++ client/app/scripts/controllers/basket.js
@@ -13,15 +13,23 @@ angular.module('srScopingApp')
       basket: []
     });
 
-    $scope.basket = $scope.$storage.basket;
-
-    $scope.basket.total = function() {
+    $scope.total = function() {
       var total = 0;
 
-      angular.forEach($scope.basket, function(task) {
+      if ($scope.$storage.basket <= 0) {
+        return total;
+      }
+
+      angular.forEach($scope.$storage.basket, function(task) {
         total += task.sor.Price * task.quantity;
       });
 
       return total;
+    };
+
+    $scope.clearBasket = function() {
+      delete $scope.$storage.basket;
+
+      $scope.$storage.basket = [];
     };
   });
