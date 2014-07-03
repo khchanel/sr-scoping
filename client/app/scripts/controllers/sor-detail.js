b3920e08800236f7436d837d6acc51fbebcc48a8
Nelson Chan <khchanel@gmail.com>
2014-07-03 16:13:26 +1000

    Implement add task to basket Squashed commit of the following:
    commit cad3a058f96984a9916b1f67bd88709762a7d7a3
    Author: Nelson Chan <khchanel@gmail.com>
    Date:   Thu Jul 3 16:12:08 2014 +1000
    
        [basket] remove unnecessary validation in addTask()
    
    commit c551bf9e11b7df5991ea124c2757ebdd5eb5a746
    Author: Nelson Chan <khchanel@gmail.com>
    Date:   Thu Jul 3 16:11:38 2014 +1000
    
        [basket] refine sor detail add task button
    
    commit fced28fd424e23c3910206ceefc1461f1c2322dd
    Author: Nelson Chan <khchanel@gmail.com>
    Date:   Thu Jul 3 15:49:50 2014 +1000
    
        [basket] clean up comment
    
    commit 1477585cc15999c8ed174a3c89e15bdf88c7792e
    Author: Nelson Chan <khchanel@gmail.com>
    Date:   Thu Jul 3 15:37:23 2014 +1000
    
        [basket] implement add task to sor detail page
    
    commit 3edc27b513337636d566dc40508c014b5f60a991
    Author: Nelson Chan <khchanel@gmail.com>
    Date:   Wed Jul 2 15:17:07 2014 +1000
    
        tmp add task

--- client/app/scripts/controllers/sor-detail.js
+++ client/app/scripts/controllers/sor-detail.js
@@ -8,7 +8,34 @@
  * Controller of the srScopingApp
  */
 angular.module('srScopingApp')
-  .controller('SorDetailCtrl', function ($scope) {
+  .controller('SorDetailCtrl', function($scope, $localStorage) {
+    $scope.$storage = $localStorage;
+
     $scope.sor = $scope.ons.navigator.getCurrentPage().options.sorObj;
-    $scope.quantity = null;
+    $scope.quantity = 0;
+    $scope.addBtnClass = 'topcoat-button';
+    $scope.addBtnText = 'Add';
+    $scope.saved = false;
+
+
+    /**
+     * addTask()
+     * function to add task to basket storage
+     */
+    $scope.addTask = function() {
+
+      // set task data
+      var task = {
+        'sor': $scope.sor,
+        'quantity': $scope.quantity
+      };
+
+      // add task to basket
+      $scope.$storage.basket.push(task);
+
+      // disable the add button to avoid re-add
+      $scope.addBtnText = 'Added';
+      $scope.addBtnClass = 'topcoat-button--quiet';
+      $scope.saved = true;
+    };
   });
