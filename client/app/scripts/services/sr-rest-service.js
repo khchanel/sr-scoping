2aa4b9a5473989f019cfc71eb911e3d5e678b16c
Nelson Chan <khchanel@gmail.com>
2014-07-08 16:25:14 +1000

    sr rest service attach user&passwd to API request

--- client/app/scripts/services/sr-rest-service.js
+++ client/app/scripts/services/sr-rest-service.js
@@ -7,11 +7,18 @@
  * # Sor
  * Factory in the SrRestService.
  */
-angular.module('SrRestService', ['ngResource'])
-  .factory('Sor', ['$resource',
-    function ($resource) {
-      var API_SERVER = 'http://srconstruction.khchanel.com';
 
-      return $resource(API_SERVER + '/sor/:SORCode');
+angular.module('SrRestService', ['ngResource', 'ngStorage'])
+
+  .constant('SR_API_SERVER', 'http://srconstruction.khchanel.com')
+
+  .factory('Sor', ['$resource', '$localStorage', 'SR_API_SERVER',
+    function ($resource, $localStorage, SR_API_SERVER) {
+
+      return $resource(SR_API_SERVER + '/sor/:SORCode',
+        {
+          user: $localStorage.user.name,
+          passwd: $localStorage.user.passwd
+        });
     }
   ]);
