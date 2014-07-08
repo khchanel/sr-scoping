7b2ae194692fdc3061663403c5d5c98c5eabf5e2
Nelson Chan <khchanel@gmail.com>
2014-07-08 18:21:49 +1000

    [test] implement sor detail unit test

--- client/test/spec/controllers/sor-detail.js
+++ client/test/spec/controllers/sor-detail.js
@@ -8,15 +8,49 @@ describe('Controller: SorDetailCtrl', function () {
   var SorDetailCtrl,
     scope;
 
+  var testSor = {
+    SORCode: 'MIN18350',
+    Tradecode: '',
+    UomCode: 'm.',
+    Name: '(Renew strip flooring up to 6 metres)',
+    LongDescription: 'Remove and dispose of existing and supply and fix strip flooring up to 6 metres.  Hardwood\/Cypress up to 100mm wide.  Staggered joints - Each room.',
+    Status: 'False',
+    Price201213: '9.999999999',
+    Price: '9.999999999',
+    Warranty: '0',
+    Manual: '0',
+    Deleted: '0',
+    Code: '606',
+    Location: 'All,BED1 ,HALL, AIRL',
+    Photo: '~\\Files\\SOR\\1c8dbe4709ae44a388f6dee379a8f0bc.jpg'
+  };
+
+
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
+
     scope = $rootScope.$new();
+
+    // mock a ons object
+    scope.ons = {
+      navigator: {
+        getCurrentPage: function() {
+          return {
+            options: {
+              sorObj: testSor
+            }
+          };
+        }
+      }
+    };
+
+    // intantial controller
     SorDetailCtrl = $controller('SorDetailCtrl', {
       $scope: scope
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it ('should show all sor content', function () {
+    expect(scope.sor.SORCode).toBe(testSor.SORCode);
   });
 });
