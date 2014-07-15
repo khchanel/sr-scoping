bfe37c63a4c5dbe0b51de96d2fbd996766bf4c41
Miles Lee <mileslee@Miless-MacBook-Pro.local>
2014-07-15 23:35:11 +1000

    Add the project list to the menu side; Add the controller for this list#11

---
+++ client/app/scripts/controllers/proj-list.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name srScopingApp.controller:ProjCtrl
+ * @description
+ * # ProjCtrl
+ * Controller of the srScopingApp
+ */
+angular.module('srScopingApp')
+  .controller('ProjCtrl', function ($scope) {
+    $scope.testThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
