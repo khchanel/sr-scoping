95ceffc21b25f1928f1358c1a1e861a22f0e395e
Miles Lee <mileslee@sarang-pc.srcon.local>
2014-07-17 12:49:50 +1000

    delete the redundant codes in main.js

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -9,12 +9,6 @@
  */
 angular.module('srScopingApp')
   .controller('MainCtrl', ['$scope','Project', function ($scope, Project) {
-    /*
-    $http.get('http://120.151.95.114:8080/projects/services/projects.svc/GetProjectsMethod/inputStr/Nelson/Nelson@2014').
-        success(function(data) {
-            $scope.projects = data;
-        });
-    */
 
     $scope.projects = Project.query();
 
