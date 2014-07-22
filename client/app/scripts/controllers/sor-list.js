d97655fa6d83bcabf49b2c7ef0145c2ba6202496
Nelson Chan <khchanel@gmail.com>
2014-07-22 17:54:21 +1000

    rework sor list to use ngGrid

--- client/app/scripts/controllers/sor-list.js
+++ client/app/scripts/controllers/sor-list.js
@@ -8,6 +8,34 @@
  * Controller of the srScopingApp
  */
 angular.module('srScopingApp')
-  .controller('SorListCtrl', ['$scope', 'Sor', function ($scope, Sor) {
-    $scope.sors = Sor.query();
-  }]);
+  .controller('SorListCtrl', ['$scope', 'Sor',
+    function($scope, Sor) {
+
+      $scope.sors = Sor.query();
+
+      $scope.filterOptions = {
+        filterText: ''
+      };
+
+      $scope.gridOptions = {
+        data: 'sors',
+
+        columnDefs: [{
+          field: 'SORCode',
+          displayName: 'Code'
+        }, {
+          field: 'Name',
+          displayName: 'Name'
+        }, {
+          field: 'Price',
+          displayName: 'Price',
+          cellFilter: 'currency: ""'
+        }],
+
+        multiSelect: false,
+
+        filterOptions: $scope.filterOptions
+      };
+
+    }
+  ]);
