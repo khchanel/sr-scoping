2835dfcad7c8207ec88d8b218240a6a53bbae492
Nelson Chan <khchanel@gmail.com>
2014-07-22 12:08:06 +1000

    project: implement unit test case

--- client/test/spec/controllers/project.js
+++ client/test/spec/controllers/project.js
@@ -6,17 +6,49 @@ describe('Controller: ProjectCtrl', function () {
   beforeEach(module('srScopingApp'));
 
   var ProjectCtrl,
-    scope;
+    scope,
+    ShareProperty;
+
+  var testProject = {
+    'Address': '1\/69-71 Isabella Street, PARRAMATTA, NSW, 2150',
+    'Client': 'New South Wales Land & Housing Corporation',
+    'ClientRef': '241013',
+    'Code': 59005,
+    'ContractorRef': null,
+    'Coordinator': 'Nelson Chan',
+    'Duration': null,
+    'Finish': '03\/07\/2014',
+    'FinishDate': null,
+    'Instructions': '',
+    'MasterCode': null,
+    'SLA': null,
+    'Start': '25\/05\/2014',
+    'StartDate': null,
+    'Status': 'New',
+    'SubClient': null,
+    'TaskType': null
+  };
+
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, _ShareProperty_) {
+
+    ShareProperty = _ShareProperty_;
+    ShareProperty.set('active_project', testProject);
+
+    spyOn(ShareProperty, 'get').andCallThrough();
+
     scope = $rootScope.$new();
     ProjectCtrl = $controller('ProjectCtrl', {
       $scope: scope
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+
+  it('should have $scope.project defined with correct details', function() {
+    expect(scope.project).toBeDefined();
+    expect(scope.project.Code).toBe(testProject.Code);
+    expect(ShareProperty.get).toHaveBeenCalledWith('active_project');
   });
+
 });
