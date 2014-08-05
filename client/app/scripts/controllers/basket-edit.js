241abc480523bad32d651007eb89c4f25530c476
Nelson Chan <khchanel@gmail.com>
2014-08-05 15:28:36 +1000

    basket-edit: implement basket-edit page with save function

--- client/app/scripts/controllers/basket-edit.js
+++ client/app/scripts/controllers/basket-edit.js
@@ -8,10 +8,33 @@
  * Controller of the srScopingApp
  */
 angular.module('srScopingApp')
-  .controller('BasketEditCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('BasketEditCtrl', function ($scope, $localStorage, ShareProperty) {
+    $scope.$storage = $localStorage;
+    $scope.projectCode = ShareProperty.get('active_project').Code;
+
+    $scope.storedTask = $scope.ons.navigator.getCurrentPage().options.taskObj;
+
+    // use a local copy of task for draft
+    $scope.task = angular.copy($scope.storedTask);
+
+    $scope.saveBtnClass = 'topcoat-button';
+    $scope.saveBtnText = 'Save';
+    $scope.saved = false;
+
+
+    /**
+     * saveTask()
+     * function to save task to basket storage
+     */
+    $scope.saveTask = function() {
+
+      // set updated task data
+      $scope.storedTask.comment = $scope.task.comment;
+      $scope.storedTask.quantity = $scope.task.quantity;
+
+      // disable the save button to avoid re-saving
+      $scope.saveBtnText = 'Saved!';
+      $scope.saveBtnClass = 'topcoat-button--quiet';
+      $scope.saved = true;
+    };
   });
